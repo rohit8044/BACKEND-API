ebfaeb9a972ec4daf37e3b14f94417f1cf15c7d4
rohit8044 <trickcric2@gmail.com>
2026-06-03 22:35:15 -0700

    Update Modules.js

--- Module/Modules.js
+++ Module/Modules.js
@@ -3,7 +3,7 @@ const { route } = require('../Routing/Routings');
 
 class Student{
 
-    static async SendOTP(phone) {
+    static async SendOTP(phone,state) {
 
         const { data, error } = await supabase.auth.signInWithOtp({ phone });
 
